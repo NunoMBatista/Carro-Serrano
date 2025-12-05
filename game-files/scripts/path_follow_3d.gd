903ad8916a3dfb4585a1737df3941c92e29057b2
MiguelCastela <125566095+MiguelCastela@users.noreply.github.com>
2025-12-05 15:54:22 +0000

    terreno 2

--- game-files/scripts/path_follow_3d.gd
+++ game-files/scripts/path_follow_3d.gd
@@ -1,6 +1,16 @@
 extends PathFollow3D
 
+<<<<<<< Updated upstream
 @export var speed = 1.0
+=======
+@export var cur_speed := 0.0
+
+const MAX_SPEED := 10
+const ACCEL_STRENGTH := 3.5
+const BRAKE_STRENGTH := 6.0
+const MIN_SPEED := 0.0
+const FULLSTOP_SPEED := 0.5     # Speed at which the car suddenly stops and bounces back
+>>>>>>> Stashed changes
 
 func _process(delta):
 	progress += speed * delta
